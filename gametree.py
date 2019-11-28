d92845f26e2feac16e53326a017c8fd5a8402b9d
deadsmond <deadsmond@users.noreply.github.com>
2019-11-28 11:48:48 +0100

    update

--- gametree.py
+++ gametree.py
@@ -1,71 +1,86 @@
+import json
 
 
 # game tree object
 class GameTree:
     # procedure of printing object properties
     def __repr__(self):
-        # for node in self._nodes:
-        #     print(node)
-        return str(self._nodes)
+        # return json.dumps(self.__dict__)
+        return json.dumps(self.__dict__, indent=4)
 
     # initialize object
     def __init__(self):
         self._nodes = {
             'root': {
-                'id': 'root',
                 'value': 0,
                 'parents': {},
                 'children': {},
-                'probability': 1
+                'probability': 1,
+                'branch': {
+                    'value': 0,
+                    'probability': 1
+                }
             }
         }
         # list of lists of knowledge groups
         self._groups = []
+        # dictionary of leafs
+        self._leafs = []
 
     # add node method
-    def add_node(self, node, override=False):
+    def add_node(self, node: dict, override=False):
         # check if it is not overriding existing node
-        if node['id'] in self._nodes and not override:
-            raise ValueError('wrong id of node with no override permission')
-        # add node
-        self._load_node_data(node)
+        if node.get('id') is not None:
+            if node['id'] in self._nodes and not override:
+                raise ValueError('wrong id of node with no override permission')
+        else:
+            raise ValueError('no id for node provided')
 
-    def _load_node_data(self, node):
+        # add node:
         # append node to list
-        self._nodes[node.id] = node
+        id_ = node['id']
+        del node['id']
+
+        # default values for node
+        node['value'] = 0 if node.get('value') is None else node['value']
+        node['parents'] = {} if node.get('parents') is None else node['parents']
+        node['children'] = {} if node.get('children') is None else node['children']
+        node['probability'] = 1 if node.get('probability') is None else node['probability']
+        node['branch'] = {} if node.get('branch') is None else node['branch']
+        node['branch']['value'] = 0 if node['branch'].get('value') is None else node['branch']['value']
+        node['branch']['probability'] = 0 \
+            if node['branch'].get('probability') is None else node['branch']['probability']
+
+        self._nodes[id_] = node
         # add parenthood
-        self._nodes[node.parents]['children'] = node
+        for parent in node['parents']:
+            self._nodes[parent]['children'] = node
 
         # calculate total probability of node
 
         # calculate total node's value
 
+    # return list of leafs
     def get_leafs(self):
-        return dict(node['id'] for node in self._nodes if not node['children'])
+        self._leafs = [node for node in self._nodes if not self._nodes[node]['children']]
 
 
 # testing section
 if __name__ == '__main__':
+
     # plant a tree
     tree = GameTree()
     # add nodes
-    tree.add_node(
-        {
+    tree.add_node({
             'id': '1',
             'value': 2,
-            'parents': {'root'},
-            'children': {},
-            'probability': 1
-        }
-    )
-    tree.add_node(
-        {
+            'parents': {'root'}
+    })
+    tree.add_node({
             'id': '2',
             'value': 5,
-            'parents': {'root'},
-            'children': {},
-            'probability': 1
-        }
-    )
+            'parents': {'root'}
+    })
 
-    print(tree)
+    # WTF TODO
+    # print(tree)
